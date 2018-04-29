d50e490f751d7605c970276cd276195a797e3d14
slaughterwind <30817462+slaughterwind@users.noreply.github.com>
2018-04-29 15:31:41 +0800

    setting

---
+++ client/pages/setting/setting.js
@@ -0,0 +1,50 @@
+var sliderWidth = 96; // 需要设置slider的宽度，用于计算中间位置
+
+Page({
+  data: {
+    tabs: ["个人", "公司", "店铺","岗位"],
+    activeIndex: 0,
+    sliderOffset: 0,
+    sliderLeft: 0,
+    inputShowed: false,
+    inputVal: ""
+  },
+  onLoad: function () {
+    var that = this;
+    wx.getSystemInfo({
+      success: function (res) {
+        that.setData({
+          sliderLeft: (res.windowWidth / that.data.tabs.length - sliderWidth) / 2,
+          sliderOffset: res.windowWidth / that.data.tabs.length * that.data.activeIndex
+        });
+      }
+    });
+  },
+  tabClick: function (e) {
+    this.setData({
+      sliderOffset: e.currentTarget.offsetLeft,
+      activeIndex: e.currentTarget.id
+    });
+  },
+  showInput: function () {
+    this.setData({
+      inputShowed: true
+    });
+  },
+  hideInput: function () {
+    this.setData({
+      inputVal: "",
+      inputShowed: false
+    });
+  },
+  clearInput: function () {
+    this.setData({
+      inputVal: ""
+    });
+  },
+  inputTyping: function (e) {
+    this.setData({
+      inputVal: e.detail.value
+    });
+  }
+});
