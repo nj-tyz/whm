e01210a15083e686d3f073b0a1d211a3e8729072
CherryCri <1046566159@qq.com>
2018-04-29 15:09:45 +0800

    国际化中英文

--- client/lan/lan_en.js
+++ client/lan/lan_en.js
@@ -1,3 +1,108 @@
-{
-  product_add_name:"p;adu  slfkslfsl;k "
+var lan = {
+  name: "name",
+  product: "product",
+  serial_number: "serial number",
+  out_put: "store",
+  in_put: "deliver",
+  address: "address",
+  out_in_put: "store and deliver",
+
+  input_company_name: "Please enter company name",
+  input_no: "Please enter the serial number",
+  input_name: "Please enter a name",
+  input_price: "Please enter the price",
+  input_address: "Please enter the address",
+
+  submit: "submit",
+  submiting: "submiting...",
+  submit_fail: "Submission Failed",
+
+  system_prompt: "system hint",
+  click_return: "click to return",
+
+  get_success: "success",
+  get_fail: "fail",
+
+  request_success: "success",
+  request_fail: "fail",
+
+  uploading: "uploading",
+  img_upload_success: "Upload picture successfully",
+  img_upload_fail: "Failed to upload picture",
+
+  hint: "hint",
+  cancel: "cancel",
+  search: "search",
+  creating: "creating...",
+  equipment_get_fail: "Failed to get device information",
+  close: "close",
+
+  setting_success: "set up successfully",
+
+  bar_code: "barcode",
+  bar_code_error: "Barcode is wrong!",
+
+
+  index_logbtn: "login",
+  index_check_pending: "check pending",
+  index_no_company_name: "Please complement the company's information",
+  index_in_shop: "enter a shop",
+
+  out_put_type: "type",
+  out_put_warehouse: "warehouse",
+  out_put_position: "position",
+
+  position_navigation_bar_title: "the management of bar code inventory",
+  position_add: "add position",
+  position_add_success: "add position successfully",
+  position_add_fail: "Failed to add positiony",
+  position_get_list: "Get a list of positions",
+  position_get_list_success: 'success',
+  position_get_list_fail: 'fail',
+  position_all: "all position",
+
+
+  product_list: "product list",
+  product_add: "add product",
+  product_add_success: "Add product successfully",
+  product_add_fail: "Failed to add product",
+  product_get_loading: "Getting product data",
+  product_get_success: "success",
+  product_get_fail: "fail",
+  product_scan: "Scan products",
+
+  qrcode_error: "QR code is wrong",
+  qrcode_generator: "QR code generator",
+  qrcode_goods_shelf: "Shelf QR code",
+  qrcode_goods_shelf_info1: "Please click the QR code to save, and print and post to the corresponding shelf.",
+
+  check: "check",
+  checking: "checking...",
+  check_completed: "checking completed",
+  check_fail: "Failed to check, please try again later.",
+
+  check_company_name_reject: "The company has been registered by others and you can apply to join the company or change the name of another company.",
+  check_company_name_pass: "The company name is available.",
+
+  apply_join: "Applications for Membership",
+  regist: "Confirm registration",
+
+  setting_company: "Please set up the company",
+
+  shop_add_success: "Add shop successfully",
+  shop_add_fail: "Failed to add shop",
+  shop_loading: "Geting shop data",
+  shop_get_success: "Get shop data successfully",
+  shop_get_fail: "Fail to get shop data",
+
+
+  store: "warehouse",
+  store_add: "Add warehouse",
+  store_add_success: "Add warehouse successfully",
+  store_add_fail: "Fail to add warehouse",
+  store_load_local: "Get a list of this warehouse",
+  store_loading: "Getting warehouse data",
+  store_load_error1: "The warehouse is not in this shop, please choose the other shop!",
+  store_all: "all warehouse"
 }
+
